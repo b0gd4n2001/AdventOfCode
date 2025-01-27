a6e7166438a43b1be1127e1de4e9b41686672c38
b0gd4n2001 <lulelarub@gmail.com>
2025-01-27 03:02:36 +0200

    Add project files.

---
+++ HistorianHysteria/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HistorianHysteria
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] input = File.ReadAllLines("F:\\Projects\\AdventOfCode\\HistorianHysteria\\input.txt");
+            int[] firstColumn = new int[input.Length];
+            int[] secondColumn = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] line = input[i].Split("  ");
+                firstColumn[i] = int.Parse(line[0]);
+                secondColumn[i] = int.Parse(line[1]);
+            }
+
+            Array.Sort(firstColumn);
+            Array.Sort(secondColumn);
+
+            int total = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                total += Math.Abs(firstColumn[i] - secondColumn[i]);
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
