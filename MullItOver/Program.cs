b3882fb8e5aa8827ccebb84ba8b8fb8e199f763e
b0gd4n2001 <lulelarub@gmail.com>
2025-01-29 06:11:23 +0200

    day 3

---
+++ MullItOver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MullItOver
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int sum = 0;
+            string input = File.ReadAllText("F:\\Projects\\AdventOfCode\\MullItOver\\input.txt");
+            const string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+            foreach (Match match in Regex.Matches(input, pattern))
+            {
+                sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[^1].Value);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
